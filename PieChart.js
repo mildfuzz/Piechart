df3d3e7a8cb705291eef95c658104cc04cd86351
John Farrow <john@mildfuzz.com>
2014-11-17 13:40:36 +0000

    added redraw, thickness control, offset control, data processing

--- PieChart.js
+++ PieChart.js
@@ -3,9 +3,11 @@
  *
  * Build Pie chart based on percentages
  *
- * Pie(ParentElement, data);
+ * myPie = Pie(ParentElement, data, options);
  *
  * Canvas takes size from ParentElement
+ *
+ * myPie.redraw() available to recalulate size
  *
  * Data must look like
  *
@@ -24,23 +26,68 @@
  *                  "hex": "#ffaa11"
  *              }
  *          ]
+ *
+ *  options are optional
+ *
+ * options.thickness = 0.1 (default. Percentage of total radius to display)
+ * options.offset = 1 (degrees. Size of gap between segments)
  */
 
 define([], function() {
     var PieChart, pc, tests,
         edgeOrder = ['top', 'right', 'bottom', 'left'];
 
-    PieChart = function(element, data) {
-        this.initCanvas(element);
-        this.drawSegment(0, 120, '#123456');
-        this.drawSegment(120, 200, '#FFF456');
-        this.drawSegment(200, 0, '#123FFF');
+    PieChart = function(element, data, options) {
 
-        
-
+        this.element = element;
+        this.procOptions(options);
+        this.data = this.getConvertedData(data);
+        this.initCanvas();
+        this.draw();
     };
     pc = PieChart.prototype;
 
+    pc.procOptions = function(options) {
+        options = options || {};
+        this.options = {};
+        this.options.thickness = options.thickness || 0.1;
+        this.options.offset = options.offset || 1;
+    };
+    pc.draw = function() {
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.data.forEach(function(data) {
+            this.drawSegment.apply(this, data);
+        }.bind(this));
+
+        this.maskPath.apply(this, this.getRingMaskRadii(this.options.thickness));
+    };
+    pc.clearAndDraw = function() {
+        this.initCanvas();
+        this.draw();
+    };
+    pc.getRingMaskRadii = function(thickness) {
+        var radius = (this.canvas.height > this.canvas.width ? this.canvas.width : this.canvas.height) / 2,
+            thicknessRadius = radius * (1 - thickness);
+
+        return [radius, thicknessRadius];
+    };
+    pc.getAngleFromPercentage = function(percentage) {
+        return percentage * 360;
+    };
+    pc.getConvertedData = function(data) {
+        var currentStartAngle = 0;
+        return data
+            .map(function(data) {
+                var startAngle = currentStartAngle;
+                var endAngle = startAngle + this.getAngleFromPercentage(data.percentage);
+                currentStartAngle = endAngle;
+                return [startAngle, endAngle, data.hex];
+            }.bind(this))
+            .map(function(data) {
+                return [data[0] + (this.options.offset / 2), data[1] - (this.options.offset / 2), data[2]];
+            }.bind(this));
+    };
     pc.getCorner = function(edge1, edge2) {
         return [edge1, edge2].splice(0, 2).map(function(edge) {
             switch (edge) {
@@ -53,22 +100,23 @@ define([], function() {
                 case 'left':
                     return 0;
             }
-        }.bind(this))
-    }
-
-    pc.initCanvas = function(element) {
-        var sizes = element.getBoundingClientRect();
-        this.canvas = document.createElement('canvas');
+        }.bind(this));
+    };
+    pc.initCanvas = function() {
+        var sizes = this.element.getBoundingClientRect()
+            addedToCanvas = !!this.canvas;
+        this.canvas = this.canvas || document.createElement('canvas');
         this.canvas.height = sizes.height;
         this.canvas.width = sizes.width;
-        element.appendChild(this.canvas);
+        this.ctx = this.canvas.getContext('2d');
+        addedToCanvas || this.element.appendChild(this.canvas);
     };
-    pc.getCentre = function(){
+    pc.getCentre = function() {
         return [this.canvas.width / 2, this.canvas.height / 2];
     };
     pc.getOpposite = function(angle, adjacent) {
         var args = this.angleOffset(angle);
-        args.unshift(adjacent)
+        args.unshift(adjacent);
         return this.getLength.apply(this, args);
     };
     pc.getLength = function(adjacent, angle, inverted) {
@@ -77,9 +125,6 @@ define([], function() {
     };
     pc.getEndOfLine = function(angle) {
         var edge = this.getEdge(angle);
-
-
-
         switch (edge) {
             case 'top' :
                 return [this.getCentre()[0] + this.getOpposite(angle, this.getCentre()[1]), 0];
@@ -90,9 +135,6 @@ define([], function() {
             case 'left' :
                 return [0, this.getCentre()[1] - this.getOpposite(angle, this.getCentre()[0])];
         }
-
-
-
     };
 
 
@@ -115,69 +157,47 @@ define([], function() {
             case 5 : return 'left'; case 6 : return 'left';
         }
     };
-    pc.drawLine = function(segment, angle, color) {
+    pc.drawLine = function(angle, color) {
         var endPoint = this.getEndOfLine(angle);
-        segment.beginPath();
-        segment.moveTo.apply(segment, this.getCentre());
-        segment.lineTo.apply(segment, endPoint);
-        segment.strokeStyle = color;
-        segment.stroke();
-        
+        this.ctx.beginPath();
+        this.ctx.moveTo.apply(this.ctx, this.getCentre());
+        this.ctx.lineTo.apply(this.ctx, endPoint);
+
         return {
             point: endPoint,
-            edge: this.getEdge(angle),
-            segment: segment
+            edge: this.getEdge(angle)
         };
     };
     pc.drawSegment = function(startAngle, endAngle, color) {
-       
-
-
         var startLine, endLine,
-            segment = this.canvas.getContext('2d'), i = 0;
+            i = 0;
+        startLine = this.drawLine(startAngle, color);
+        endLine = this.drawLine(endAngle, color);
 
 
-        
 
-        // segment.arc(this.getCentre()[0], this.getCentre()[1],this.canvas.height/2 - 20,0,Math.PI*2,true);
-        // segment.globalCompositeOperation = "destination-out";
-        // segment.clip();
-
-        startLine = this.drawLine(segment, startAngle, color);
-        endLine = this.drawLine(segment, endAngle, color);
-
-
-        
         while (startLine.edge !== endLine.edge) {
             endLine = this.gotoPreviousCorner(endLine);
         }
-        segment.lineTo.apply(segment, startLine.point);
-        segment.fillStyle = color;
-        segment.fill();
-
-        segment.beginPath();
-        segment.arc(this.getCentre()[0], this.getCentre()[1],this.canvas.height / 2 - 20, 0, Math.PI * 2, true);
-        segment.closePath();
+        this.ctx.lineTo.apply(this.ctx, startLine.point);
+        this.ctx.fillStyle = color;
+        this.ctx.fill();
+    };
+    pc.maskPath = function(radius, thickness) {
+        this.ctx.beginPath();
+        this.ctx.arc(this.getCentre()[0], this.getCentre()[1], thickness, 0, Math.PI * 2, true);
+        this.ctx.closePath();
 
         /// set composite mode
-        segment.globalCompositeOperation = 'destination-out';
-        segment.fill();
+        this.ctx.globalCompositeOperation = 'destination-out';
+        this.ctx.fill();
 
-        segment.arc(this.getCentre()[0], this.getCentre()[1],this.canvas.height/2,0,Math.PI*2,true);
-        segment.globalCompositeOperation = 'destination-in';
-        segment.fill();
+        this.ctx.arc(this.getCentre()[0], this.getCentre()[1], radius, 0, Math.PI * 2, true);
+        this.ctx.globalCompositeOperation = 'destination-in';
+        this.ctx.fill();
 
         /// reset composite mode to default
-        segment.globalCompositeOperation = 'source-over';
-
-        
-        // segment.restore()
-
-
-
-
-        
-
+        this.ctx.globalCompositeOperation = 'source-over';
     };
     pc.gotoPreviousCorner = function(line) {
         var newEdgeIndex = (edgeOrder.indexOf(line.edge) || 4) - 1,
@@ -187,19 +207,14 @@ define([], function() {
 
             cornerPoint = this.getCorner(xEdge, yEdge);
 
-            line.segment.lineTo.apply(line.segment, cornerPoint);
+            this.ctx.lineTo.apply(this.ctx, cornerPoint);
 
             line.edge = edgeOrder[newEdgeIndex];
             line.point = cornerPoint;
 
 
         return line;
-    }
-
-
-
-
-
+    };
     tests = function(element, data) {
         var result = true;
 
@@ -211,17 +226,18 @@ define([], function() {
                 throw new TypeError('Pie Data invalid, must be an array of objects -> [{hex: "string", percentage: number}]');
                 result = false;
             }
-            if (data.reduce(function(previousValue, currentValue, index, array) {return previousValue + currentValue.percentage;},0) > 100) {
-                throw new RangeError('Sum value of data percentages must not exceed 100');
+            if (data.reduce(function(previousValue, currentValue, index, array) {return previousValue + currentValue.percentage;},0) > 1) {
+                throw new RangeError('Sum value of data percentages must not exceed 1');
                 result = false;
             }
 
         return result;
     };
 
-    return function(element, data) {
+    return function(element, data, options) {
         if (tests.apply(this, arguments)) {
-            var Pie = new PieChart(element, data);
+            var Pie = new PieChart(element, data, options);
+            Pie.redraw = Pie.clearAndDraw.bind(Pie); //alias for external use
             return Pie;
         }
 
